ae8c6115bc7f3eeac7fb2656d9850ca8a4fd323d
Alexsander Kafka <alexsanderkafka2001alex@gmail.com>
2023-06-15 15:08:21 -0300

    Lógica de troca de tela

--- app/src/main/java/com/alex/teste/appcaracoroa/ResultadoActivity.java
+++ app/src/main/java/com/alex/teste/appcaracoroa/ResultadoActivity.java
@@ -2,13 +2,32 @@ package com.alex.teste.appcaracoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    private Button buttonVolta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        buttonVolta = findViewById(R.id.buttonVoltar);
+
+        buttonVolta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
